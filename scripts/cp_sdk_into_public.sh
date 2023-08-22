d7c7e17a3dc30d59df083638918c9d063f08bc35
Richard Wu <richardwu1997@gmail.com>
2023-08-21 20:19:42 -0400

    Updated cp sdk script.

--- scripts/cp_sdk_into_public.sh
+++ scripts/cp_sdk_into_public.sh
@@ -1,20 +1,27 @@
+dest=../tcomp-sdk
+
 # delete old
-rm -rf ../tcomp-sdk/src
-rm ../tcomp-sdk/package.json
-rm ../tcomp-sdk/yarn.lock
-rm ../tcomp-sdk/tsconfig.json
+rm -rf ${dest}/src
+rm ${dest}/package.json
+rm ${dest}/yarn.lock
+rm ${dest}/tsconfig.json
 
 # add new
-cp -r ./src ../tcomp-sdk/src
-cp package.json ../tcomp-sdk/package.json
-cp yarn.lock ../tcomp-sdk/yarn.lock
-cp tsconfig.json ../tcomp-sdk/tsconfig.json
+cp -r ./src ${dest}/src
+cp package.json ${dest}/package.json
+cp yarn.lock ${dest}/yarn.lock
+cp tsconfig.json ${dest}/tsconfig.json
 
 # fix the new package json
-sed -i '' -e 's/tcomp-ts/tcomp-sdk/g' ../tcomp-sdk/yarn.lock
-sed -i '' -e 's/tcomp-ts/tcomp-sdk/g' ../tcomp-sdk/package.json
-sed -i '' -e 's/@tensor-hq\/tcomp-sdk/@tensor-oss\/tcomp-sdk/g' ../tcomp-sdk/package.json
-sed -i '' -e 's/tcomp.git/tcomp-sdk.git/g' ../tcomp-sdk/package.json
-sed -i '' -e 's/https:\/\/npm.pkg.github.com/https:\/\/registry.npmjs.org/g' ../tcomp-sdk/package.json
-sed -i '' -e 's/pubpush/publish:public/g' ../tcomp-sdk/package.json
-sed -i '' -e 's/yarn npm publish && yarn push/npm publish --access public/g' ../tcomp-sdk/package.json
+sed -i '' -e 's/tcomp-ts/tcomp-sdk/g' ${dest}/yarn.lock
+sed -i '' -e 's/tcomp-ts/tcomp-sdk/g' ${dest}/package.json
+sed -i '' -e 's/@tensor-hq\/tcomp-sdk/@tensor-oss\/tcomp-sdk/g' ${dest}/package.json
+sed -i '' -e 's/tcomp.git/tcomp-sdk.git/g' ${dest}/package.json
+sed -i '' -e 's/https:\/\/npm.pkg.github.com/https:\/\/registry.npmjs.org/g' ${dest}/package.json
+sed -i '' -e 's/pubpush/publish:public/g' ${dest}/package.json
+sed -i '' -e 's/yarn npm publish && yarn push/npm publish --access public --registry https:\/\/registry.npmjs.org/g' ${dest}/package.json
+sed -i '' -e 's/ && yarn tsc --p tsconfig.tests.json//g' ${dest}/package.json
+
+pushd ${dest}
+yarn
+popd
