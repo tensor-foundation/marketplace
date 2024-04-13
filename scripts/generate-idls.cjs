9effe8ed4fc2370fdd7497db41ce9ab64e70bd64
febo <febo@tensor.so>
2024-04-13 17:28:40 +0100

    Debugging

--- scripts/generate-idls.cjs
+++ scripts/generate-idls.cjs
@@ -1,7 +1,6 @@
 const path = require("path");
 const { generateIdl } = require("@metaplex-foundation/shank-js");
 
-const idlDir = path.join(__dirname, "..", "programs");
 const binaryInstallDir = path.join(__dirname, "..", ".crates");
 const programDir = path.join(__dirname, "..", "programs");
 
@@ -9,18 +8,19 @@ generateIdl({
   generator: "anchor",
   programName: "marketplace_program",
   programId: "TCMPhJdwDryooaGtiocG1u3xcYbRpiJzb283XfCZsDp",
-  idlDir: path.join(idlDir, "marketplace"),
+  idlDir: path.join(programDir, "marketplace"),
   idlName: "idl",
   binaryInstallDir,
   programDir: path.join(programDir, "marketplace"),
 });
-
+/*
 generateIdl({
   generator: "anchor",
   programName: "cpi_test",
   programId: "5zABSn1WYLHYenFtTFcM5AHdJjnHkx6S85rkWkFzLExq",
-  idlDir: path.join(idlDir, "cpi-test"),
+  idlDir: path.join(programDir, "cpi-test"),
   idlName: "idl",
   binaryInstallDir,
   programDir: path.join(programDir, "cpi-test"),
 });
+*/
