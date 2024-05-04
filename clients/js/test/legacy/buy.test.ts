06fbcde9717ed3e809af62eda8c67b7b1d38b5e6
febo <febo@tensor.so>
2024-05-04 01:42:20 +0100

    Add cosigner tests

--- clients/js/test/legacy/buy.test.ts
+++ clients/js/test/legacy/buy.test.ts
@@ -5,6 +5,7 @@ import {
   assertAccountExists,
   fetchEncodedAccount,
   fetchJsonParsedAccount,
+  generateKeyPairSigner,
   pipe,
 } from '@solana/web3.js';
 import {
@@ -217,6 +218,135 @@ test('it cannot buy a legacy pNFT with a lower amount', async (t) => {
       cause: {
         context: {
           code: TensorMarketplaceProgramErrorCode.PRICE_MISMATCH,
+        },
+      },
+    });
+  }
+});
+
+test('it can buy a legacy NFT with a cosigner', async (t) => {
+  const client = createDefaultSolanaClient();
+  const owner = await generateKeyPairSignerWithSol(client);
+
+  // We create an NFT.
+  const { mint } = await createDefaultNft(client, owner, owner, owner);
+
+  // And we list the NFT with a cosigner.
+  const cosigner = await generateKeyPairSigner();
+  const listLegacyIx = await getListLegacyInstructionAsync({
+    owner,
+    mint,
+    amount: 1,
+    cosigner,
+  });
+
+  await pipe(
+    await createDefaultTransaction(client, owner),
+    (tx) => appendTransactionInstruction(listLegacyIx, tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  const [listing] = await findListStatePda({ mint });
+  assertAccountExists(await fetchEncodedAccount(client.rpc, listing));
+
+  // When a buyer buys the NFT.
+  const buyer = await generateKeyPairSignerWithSol(client);
+  const buyLegacyIx = await getBuyLegacyInstructionAsync({
+    owner: owner.address,
+    payer: buyer,
+    mint,
+    maxAmount: 2,
+    cosigner,
+    creators: [owner.address],
+  });
+
+  await pipe(
+    await createDefaultTransaction(client, buyer),
+    (tx) => appendTransactionInstruction(buyLegacyIx, tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  // Then the listing account should have been closed.
+  t.false((await fetchEncodedAccount(client.rpc, listing)).exists);
+
+  // And the listing token account should have been closed.
+  t.false(
+    (
+      await fetchEncodedAccount(
+        client.rpc,
+        (await findAtaPda({ mint, owner: listing }))[0]
+      )
+    ).exists
+  );
+
+  // And the buyer has the NFT.
+  const buyerToken = await fetchJsonParsedAccount(
+    client.rpc,
+    (await findAtaPda({ mint, owner: buyer.address }))[0]
+  );
+  assertAccountDecoded(buyerToken);
+
+  t.like(buyerToken, {
+    data: {
+      tokenAmount: { amount: '1' },
+    },
+  });
+});
+
+test('it cannot buy a legacy pNFT with a missing cosigner', async (t) => {
+  const client = createDefaultSolanaClient();
+  const owner = await generateKeyPairSignerWithSol(client);
+  // We create an NFT.
+  const { mint } = await createDefaultpNft(client, owner, owner, owner);
+
+  // And we list the NFT with a cosigner.
+  const cosigner = await generateKeyPairSigner();
+  const listLegacyIx = await getListLegacyInstructionAsync({
+    owner,
+    mint,
+    amount: 10,
+    cosigner,
+    tokenStandard: TokenStandard.ProgrammableNonFungible,
+  });
+
+  await pipe(
+    await createDefaultTransaction(client, owner),
+    (tx) => appendTransactionInstruction(listLegacyIx, tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  const [listing] = await findListStatePda({ mint });
+  assertAccountExists(await fetchEncodedAccount(client.rpc, listing));
+
+  // When a buyer tries to buy the NFT with a lower amount.
+  const buyer = await generateKeyPairSignerWithSol(client);
+  const buyLegacyIx = await getBuyLegacyInstructionAsync({
+    owner: owner.address,
+    payer: buyer,
+    mint,
+    maxAmount: 10,
+    tokenStandard: TokenStandard.ProgrammableNonFungible,
+    creators: [owner.address],
+  });
+
+  const computeIx = getSetComputeUnitLimitInstruction({
+    units: 300_000,
+  });
+
+  // Then we expect an error.
+  try {
+    await pipe(
+      await createDefaultTransaction(client, buyer),
+      (tx) => appendTransactionInstruction(computeIx, tx),
+      (tx) => appendTransactionInstruction(buyLegacyIx, tx),
+      (tx) => signAndSendTransaction(client, tx)
+    );
+    t.fail('Expected bad cosigner error.');
+  } catch (error) {
+    t.like(error, {
+      cause: {
+        context: {
+          code: TensorMarketplaceProgramErrorCode.BAD_COSIGNER,
         },
       },
     });
--- clients/js/test/legacy/list.test.ts
+++ clients/js/test/legacy/list.test.ts
@@ -1,4 +1,4 @@
-import { appendTransactionInstruction, pipe } from '@solana/web3.js';
+import { appendTransactionInstruction, generateKeyPairSigner, pipe } from '@solana/web3.js';
 import {
   createDefaultSolanaClient,
   createDefaultTransaction,
@@ -45,6 +45,7 @@ test('it can list a legacy NFT', async (t) => {
       owner: owner.address,
       amount: 1n,
       assetId: mint,
+      cosigner: null,
     },
   });
 });
@@ -78,6 +79,78 @@ test('it can list a legacy Programmable NFT', async (t) => {
       owner: owner.address,
       amount: 1n,
       assetId: mint,
+      cosigner: null,
+    },
+  });
+});
+
+test('it can list a legacy NFT with a cosigner', async (t) => {
+  const client = createDefaultSolanaClient();
+  const owner = await generateKeyPairSignerWithSol(client);
+  // We create an NFT.
+  const { mint } = await createDefaultNft(client, owner, owner, owner);
+
+  const cosigner = await generateKeyPairSigner();
+  const listLegacyIx = await getListLegacyInstructionAsync({
+    owner,
+    mint,
+    amount: 1,
+    cosigner,
+  });
+
+  // When we list the NFT with a cosigner.
+  await pipe(
+    await createDefaultTransaction(client, owner),
+    (tx) => appendTransactionInstruction(listLegacyIx, tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  // Then we should be able to fetch the listing.
+  const listing = await fetchListStateFromSeeds(client.rpc, {
+    mint,
+  });
+  t.like(listing, <ListState>{
+    data: {
+      owner: owner.address,
+      amount: 1n,
+      assetId: mint,
+      cosigner: cosigner.address,
+    },
+  });
+});
+
+test('it can list a legacy Programmable NFT with a cosigner', async (t) => {
+  const client = createDefaultSolanaClient();
+  const owner = await generateKeyPairSignerWithSol(client);
+  // We create an pNFT.
+  const { mint } = await createDefaultpNft(client, owner, owner, owner);
+
+  const cosigner = await generateKeyPairSigner();
+  const listLegacyIx = await getListLegacyInstructionAsync({
+    owner,
+    mint,
+    amount: 1,
+    tokenStandard: TokenStandard.ProgrammableNonFungible,
+    cosigner,
+  });
+
+  // When we list the pNFT.
+  await pipe(
+    await createDefaultTransaction(client, owner),
+    (tx) => appendTransactionInstruction(listLegacyIx, tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  // Then we should be able to fetch the listing.
+  const listing = await fetchListStateFromSeeds(client.rpc, {
+    mint,
+  });
+  t.like(listing, <ListState>{
+    data: {
+      owner: owner.address,
+      amount: 1n,
+      assetId: mint,
+      cosigner: cosigner.address,
     },
   });
 });
