6a121f2e73735e8daf443701a1c2de03be2b5a6e
leantOnSol <leantOnSol@proton.me>
2024-07-07 23:44:46 +0200

    add bid examples (collection/single)

---
+++ examples/make_single_NFT_bid.ts
@@ -0,0 +1,33 @@
+import { simulateTxWithIxs } from "@tensor-foundation/common-helpers";
+import { rpc, keypairBytes } from "./common";
+import { 
+    address, 
+    KeyPairSigner, 
+    createKeyPairSignerFromBytes, 
+} from "@solana/web3.js";
+import {
+    Target,
+    BidAsyncInput,
+    getBidInstructionAsync,
+} from "@tensor-foundation/marketplace";
+
+// make a bid on a single NFT specified by its mint address
+async function makeSingleNftBid(mint: string, amountLamports: number) {
+    const keypairSigner: KeyPairSigner = await createKeyPairSignerFromBytes(
+        Buffer.from(keypairBytes),
+        false,
+    );
+    const bidAsyncInput: BidAsyncInput = {
+        owner: keypairSigner,
+        target: Target.AssetId,
+        targetId: address(mint),
+        amount: amountLamports
+    }
+    // retrieve bid instruction
+    const bidIx = await getBidInstructionAsync(bidAsyncInput);
+    await simulateTxWithIxs(rpc, [bidIx], keypairSigner);
+}
+makeSingleNftBid(
+    "NFT_ADDRESS_YOU_WANT_TO_MAKE_A_BID_FOR",
+    1 * 1_000_000_000, // 1 sol
+);
