3c340f2e3ddc342b0a8d1730b05379e5b4034b3c
leantOnSol <leantOnSol@proton.me>
2024-07-26 00:58:23 +0200

    close expire coll bid

--- clients/js/test/legacy/close-expired-bid.test.ts
+++ clients/js/test/legacy/close-expired-bid.test.ts
@@ -1,6 +1,7 @@
 import {
   appendTransactionMessageInstruction,
   fetchEncodedAccount,
+  generateKeyPairSigner,
   pipe,
 } from '@solana/web3.js';
 import {
@@ -18,6 +19,8 @@ import {
   getBidInstructionAsync,
   getCloseExpiredBidInstructionAsync,
 } from '../../src/index.js';
+import { createWhitelistV2 } from './_common.js';
+import { Mode } from '@tensor-foundation/whitelist';
 
 test('it can close an expired a bid on a legacy NFT', async (t) => {
   const client = createDefaultSolanaClient();
@@ -29,10 +32,10 @@ test('it can close an expired a bid on a legacy NFT', async (t) => {
     authority: owner,
     owner,
   });
-
+  const price = 100_000_000n;
   const bidIx = await getBidInstructionAsync({
     owner,
-    amount: 1,
+    amount: price,
     target: Target.AssetId,
     targetId: mint,
     expireInSec: 1,
@@ -47,6 +50,8 @@ test('it can close an expired a bid on a legacy NFT', async (t) => {
 
   // And we wait for the listing to expire.
   await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const preCloseOwnerFunds = (await client.rpc.getBalance(owner.address).send()).value;
 
   const closeExpiredBidIx = await getCloseExpiredBidInstructionAsync({
     owner: owner.address,
@@ -70,6 +75,10 @@ test('it can close an expired a bid on a legacy NFT', async (t) => {
       )
     ).exists
   );
+
+    // And the owner got his sol back (+ account rent)
+    const postCloseOwnerFunds = (await client.rpc.getBalance(owner.address).send()).value;
+    t.assert(postCloseOwnerFunds >= preCloseOwnerFunds + price);
 });
 
 test('it cannot close an active bid on a legacy NFT', async (t) => {
@@ -121,4 +130,66 @@ test('it cannot close an active bid on a legacy NFT', async (t) => {
       },
     });
   }
+});
+
+test('it can close an expired a bid on a legacy collection', async (t) => {
+  const client = createDefaultSolanaClient();
+  const owner = await generateKeyPairSignerWithSol(client);
+  const creatorKeypair = await generateKeyPairSignerWithSol(client);
+
+  // Create whitelist
+  const { whitelist } = await createWhitelistV2({
+      client,
+      updateAuthority: creatorKeypair,
+      conditions: [{ mode: Mode.FVC, value: creatorKeypair.address }],
+  });
+
+  const bidId = (await generateKeyPairSigner()).address;
+  const price = 300_000_000n;
+  const bidIx = await getBidInstructionAsync({
+    owner,
+    amount: price,
+    target: Target.Whitelist,
+    targetId: whitelist,
+    bidId,
+    expireInSec: 1,
+  });
+
+  await pipe(
+    await createDefaultTransaction(client, owner),
+    (tx) => appendTransactionMessageInstruction(bidIx, tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  // And we wait for the listing to expire.
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const preCloseOwnerFunds = (await client.rpc.getBalance(owner.address).send()).value;
+
+  const closeExpiredBidIx = await getCloseExpiredBidInstructionAsync({
+    owner: owner.address,
+    bidId,
+  });
+
+  // When we cancel the bid with a "generic" payer.
+  const payer = await generateKeyPairSignerWithSol(client);
+  await pipe(
+    await createDefaultTransaction(client, payer),
+    (tx) => appendTransactionMessageInstruction(closeExpiredBidIx, tx),
+    (tx) => signAndSendTransaction(client, tx)
+  );
+
+  // Then the bid state should not exist.
+  t.false(
+    (
+      await fetchEncodedAccount(
+        client.rpc,
+        (await findBidStatePda({ owner: owner.address, bidId: bidId }))[0]
+      )
+    ).exists
+  );
+
+  // And the owner got his sol back (+ account rent)
+  const postCloseOwnerFunds = (await client.rpc.getBalance(owner.address).send()).value;
+  t.assert(postCloseOwnerFunds >= preCloseOwnerFunds + price);
 });
