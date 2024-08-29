55caa3e4c8adeeb1e67bc01b09fa7b0b281f1e4d
leantOnSol <leantOnSol@proton.me>
2024-08-29 18:31:57 +0200

    enable mainnet build

--- scripts/program/build.mjs
+++ scripts/program/build.mjs
@@ -2,16 +2,18 @@
 import "zx/globals";
 import { workingDirectory, getProgramFolders } from "../utils.mjs";
 
-const fetchFromArtifacts = argv._.filter(a => a !== path.basename(__filename))[0] === "artifacts";
+const whereToFetchBinariesFrom = argv._.filter(a => a !== path.basename(__filename))[0];
 
 // Save external programs binaries to the output directory.
-import "./dump_mainnet.mjs";
+import "./dump_mainnet_external.mjs";
 
-if(fetchFromArtifacts) await import("../fetch-external-binaries.mjs"); 
+// Fetch "Offchain"ProgramAddresse from artifacts/mainnet/devnet
+if(whereToFetchBinariesFrom === "artifacts") await import("../fetch-external-binaries.mjs"); 
+else if(whereToFetchBinariesFrom === "mainnet") await import("./dump_mainnet_others.mjs"); 
 else await import("./dump_devnet.mjs");
 
 // Build the programs.
 for (const folder of getProgramFolders()) {
   cd(`${path.join(workingDirectory, folder)}`);
-  await $`cargo-build-sbf ${process.argv.slice(3)}`;
+  await $`cargo-build-sbf ${process.argv.slice(4)}`;
 }
