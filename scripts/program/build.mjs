4ab856380e277601ce537caaed3de025af4ddc6c
leantOnSol <leantOnSol@proton.me>
2025-06-25 22:35:37 +0200

    adjust dump scripts + adjust CI to dump ext binaries on build

--- scripts/program/build.mjs
+++ scripts/program/build.mjs
@@ -5,12 +5,13 @@ import { workingDirectory, getProgramFolders } from "../utils.mjs";
 const whereToFetchBinariesFrom = argv._.filter(a => a !== path.basename(__filename))[0];
 
 // Save external programs binaries to the output directory.
-import "./dump_mainnet_external.mjs";
+if(whereToFetchBinariesFrom === "artifacts" || whereToFetchBinariesFrom === "mainnet") await import("./dump_mainnet_external.mjs");
+else await import("./dump_devnet_external.mjs");
 
-// Fetch "Offchain"ProgramAddresse from artifacts/mainnet/devnet
+// Fetch binaries (potentially offchain) from artifacts/mainnet/devnet
 if(whereToFetchBinariesFrom === "artifacts") await import("../fetch-external-binaries.mjs"); 
 else if(whereToFetchBinariesFrom === "mainnet") await import("./dump_mainnet_others.mjs"); 
-else await import("./dump_devnet.mjs");
+else await import("./dump_devnet_others.mjs");
 
 // Build the programs.
 for (const folder of getProgramFolders()) {
--- scripts/program/dump_devnet_external.mjs
+++ scripts/program/dump_devnet_external.mjs
@@ -1,19 +1,22 @@
 #!/usr/bin/env zx
 import "zx/globals";
 import {
+  getExternalAccountAddresses,
+  getExternalProgramAddresses,
   getExternalProgramOutputDir,
-  getOffchainProgramAddresses
 } from "../utils.mjs";
-import { dump } from "./dump.mjs"
+import { dump } from "./dump.mjs";
 
 // Get input from environment variables.
-const rpc = process.env.RPC ?? "https://api.mainnet-beta.solana.com";
+const rpc = "https://api.devnet.solana.com";
 const outputDir = getExternalProgramOutputDir();
 // Ensure we have some external programs/accounts to dump.
-const programs = getOffchainProgramAddresses();
+const programs = getExternalProgramAddresses();
+const accounts = getExternalAccountAddresses();
 const addresses = [
   ...programs.map((program) => `${program}.so`),
+  ...accounts.map((account) => `${account}.json`),
 ].flat();
 
-echo(`Dumping external accounts from mainnet to '${outputDir}':`);
+echo(`Dumping external accounts to '${outputDir}':`);
 await dump(rpc, outputDir, addresses);
